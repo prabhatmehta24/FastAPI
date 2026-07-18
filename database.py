cf0888ea1add7a5cb9ed4854afac8e4731a7aff0
Prabhat Mehta <prabhatmehta24@gmail.com>
2026-07-18 12:39:13 +0530

    Update database configuration for Render

--- database.py
+++ database.py
@@ -2,10 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres:%40Swag143341@localhost:5432/blogdb"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
